28a1250549c2de80c2382a7a433d1162595a4555
alejandrobarranco <alejandro.barranco@geoenlace.com>
2020-05-01 20:06:48 -0500

    Choosing a website: UIAlertController action sheets

--- Easy Browser/ViewController.swift
+++ Easy Browser/ViewController.swift
@@ -22,13 +22,35 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        setupUI()
         loadUrl()
+    }
+    
+    func setupUI() {
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Open", style: .plain, target: self, action: #selector(openAlertSheet))
+    }
+    
+    @objc func openAlertSheet() {
+        let vc = UIAlertController(title: "Open...", message: nil, preferredStyle: .actionSheet)
+        vc.addAction(UIAlertAction(title: "https://twitter.com", style: .default, handler: { action -> Void in
+            self.webview.load(URLRequest(url: URL(string: action.title!)!))
+        }))
+        vc.addAction(UIAlertAction(title: "https://facebook.com", style: .default, handler: { action -> Void in
+            self.webview.load(URLRequest(url: URL(string: action.title!)!))
+        }))
+        vc.addAction(UIAlertAction(title: "Cancel", style: .cancel))
+        vc.popoverPresentationController?.barButtonItem = navigationItem.rightBarButtonItem
+        present(vc, animated: true)
     }
     
     func loadUrl() {
         let url = URL(string: "https://google.com")!
         webview.load(URLRequest(url: url))
         webview.allowsBackForwardNavigationGestures = true
+    }
+    
+    func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+        title = webView.title
     }
 }
 
