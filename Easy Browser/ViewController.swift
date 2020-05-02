cbf7cbbc181c426dff59541a060ec1ab513c358e
alejandrobarranco <alejandro.barranco@geoenlace.com>
2020-05-01 19:28:17 -0500

    feat(): Creating a simple browser with wkwebview

--- Easy Browser/ViewController.swift
+++ Easy Browser/ViewController.swift
@@ -7,14 +7,31 @@
 //
 
 import UIKit
+import WebKit
 
 class ViewController: UIViewController {
 
+    var webview: WKWebView!
+    
+    override func loadView() {
+        webview = WKWebView()
+        webview.navigationDelegate = self
+        view = webview
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        loadUrl()
     }
-
-
+    
+    func loadUrl() {
+        let url = URL(string: "https://google.com")!
+        webview.load(URLRequest(url: url))
+        webview.allowsBackForwardNavigationGestures = true
+    }
 }
 
+extension ViewController : WKNavigationDelegate {
+    
+}
